e80208acc8981af7dec2ecad194f554ccb6d4ec2
lubega-deriv <lubega@deriv.com>
2024-08-02 16:51:29 +0800

    fix: update localize

--- src/utils/localize.ts
+++ src/utils/localize.ts
@@ -1,6 +1,11 @@
 import { str as crc32 } from "crc-32";
 import i18next from "i18next";
 
+/**
+ * @param {string} string
+ * @param {Record<string, unknown>} values
+ * @returns {string}
+ */
 export default function localize(
   string: string,
   values?: Record<string, unknown>
